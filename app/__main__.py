44ceb489dc34745c0be12f7b925838ea30c5c82c
achains <arthur.salio@gmail.com>
2023-09-08 17:56:36 +0300

    feat: db crud

--- app/__main__.py
+++ app/__main__.py
@@ -1,13 +1,34 @@
 import fastapi
 from fastapi import FastAPI
+from fastapi import Depends
 
 from pydantic import HttpUrl, AnyHttpUrl
-from parser import YandexMarketParser
+from sqlalchemy.orm import Session
+
+from app.parser import YandexMarketParser
+from app.db import models
+from app.db import crud
+from app.db import SessionLocal, engine
+
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.get("/summarize/")
-async def summarize(phone_page: AnyHttpUrl = ""):
+async def summarize(phone_page: AnyHttpUrl = "", db: Session = Depends(get_db)):
     parser = YandexMarketParser()
-    return phone_page
+    result = crud.create_market_item(
+        db, item_type="phone", market_name="yandex", item_name="iphone 13"
+    )
+
+    return result.id
--- app/db/crud.py
+++ app/db/crud.py
@@ -3,3 +3,9 @@ from sqlalchemy.orm import Session
 from app.db import models
 
 
+def create_market_item(db: Session, item_type: str, market_name: str, item_name: str):
+    db_market_item = models.MarketItem(type=item_type, market_name=market_name, item_name=item_name)
+    db.add(db_market_item)
+    db.commit()
+    db.refresh(db_market_item)
+    return db_market_item
